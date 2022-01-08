eda1e1c22ad160c370e890ce0065e53a19ce10cd
Son Hong Do <sondh0127@gmail.com>
2022-01-08 12:37:23 +0700

    update estimate

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -9,34 +9,34 @@ import type {
 } from 'vis-timeline/esnext'
 
 /**
- * - build time-line
- * sync data items
- * sync data groups
- *
- * learning visjs basic example
+ * 10 days
+* learning visjs basic example
  * dataset | group | item | options
  * https://visjs.github.io/vis-timeline/docs/timeline/
  * https://visjs.github.io/vis-timeline/examples/timeline/
  *
+ * - build time-line (2d)
+ * sync data items
+ * sync data groups
  * set max-zoom: 1 day
  * set min-zoom: a minute
  *
- * - build channel timeline
+ * - build channel timeline (2d)
  * event timeline(crud) | program timeline(display)
- *
  * date picker
  * next/prev day
  *
  * - integrate
- * crud item:
+ * crud item: (1d)
  * add:
  * edit:
  * delete:
+ * crud group(modal): (1d)
  *
- * crud group (modal)
+ * crud with API (2d)
  *
- * crud with API
  *
+ * TODO:
  * learn some design from https://antoniandre.github.io/vue-cal/
  * improve styling
  */
