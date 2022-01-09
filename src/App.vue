5220fb82620f66085af9d1b8cf94a1a805e805dc
Son Hong Do <sondh0127@gmail.com>
2022-01-09 23:24:40 +0700

    move config inside, add guide

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import type { DataGroup, DataItem, DataSetDataGroup, DataSetDataItem, Timeline, TimelineOptions } from 'vis-timeline/esnext'
-import type { SvelteComponent } from 'svelte'
 import EventTimeline from './components/EventTimeline.vue'
-import EventTooltip from './components/EventTooltip.svelte'
-import EventItem from './components/EventItem.svelte'
-import EventGroup from './components/EventGroup.svelte'
 
 interface TimelineExpose {
   timeline: Timeline
@@ -16,151 +12,44 @@ const eventTimelineRef = ref<TimelineExpose>()
 
 const groups = useStorage<DataGroup[]>('__GROUPS__', [])
 
-// TODO: remove items app on delete
 const items = useStorage<DataItem[]>('__ITEMS__', [])
 
-const groupsApp = ref<Record<string, SvelteComponent>>({})
-const itemsApp = ref<Record<string, SvelteComponent>>({})
-const tooltipApp = ref<SvelteComponent>()
-
-const options = ref<TimelineOptions>({
-  editable: true,
-  zoomKey: 'ctrlKey',
-  align: 'center',
-  horizontalScroll: true,
-  zoomMin: 1000 * 60 * 60,
-  zoomMax: 1000 * 60 * 60 * 24,
-  // itemsAlwaysDraggable: true,
-  showCurrentTime: true,
-  start: (() => {
-    const date = new Date()
-    date.setHours(date.getHours() - 2)
-    return date
-  })(),
-  end: (() => {
-    const date = new Date()
-    date.setHours(date.getHours() + 2)
-    return date
-  })(),
-  orientation: 'top',
-  minHeight: '150px',
-  maxHeight: '300px',
-  rollingMode: {
-    follow: true,
-    offset: 0.5,
-  },
-  template: (item, element, data) => {
-    if (!item)
-      return ''
-    const id = `item-${item.id}`
-    let containerEl = document.getElementById(id)
-    if (containerEl) {
-      itemsApp.value[item.id]?.$destroy()
-    }
-    else {
-      containerEl = document.createElement('div')
-      containerEl.id = id
-    }
-
-    itemsApp.value[item.id] = new EventItem({
-      target: containerEl,
-      props: { item },
-    })
-
-    return containerEl
-  },
-  tooltipOnItemUpdateTime: {
-    template(item) {
-      const id = `tooltip-${item.id}`
-      let containerEl = document.getElementById(id)
-      if (containerEl) {
-        tooltipApp.value?.$destroy()
-      }
-      else {
-        containerEl = document.createElement('div')
-        containerEl.id = id
-      }
-
-      tooltipApp.value = new EventTooltip({
-        target: containerEl,
-        props: { item },
-      })
-
-      return containerEl.innerHTML
-    },
-  },
-  onAdd: (item, callback) => {
-    const alert = window.prompt('Add event', item.content)
-    if (alert) {
-      item.content = alert
-      callback(item)
-    }
-    else {
-      callback(null)
-    }
-  },
-  onUpdate: (item, callback) => {
-    const alert = window.prompt('Update event', item.content)
-    if (alert) {
-      item.content = alert
-      callback(item)
-    }
-    else {
-      callback(null)
-    }
-  },
-  onMove: (item, callback) => {
-    const confirm = window.confirm('Confirm move')
-    callback(confirm ? item : null)
-  },
-  onRemove(item, callback) {
-    const confirm = window.confirm('Confirm remove')
-    callback(confirm ? item : null)
-  },
-  // groupEditable: true,
-  groupTemplate(group) {
-    if (!group)
-      return ''
-    const id = `group-${group.id}`
-    let containerEl = document.getElementById(id)
-    if (containerEl) {
-      groupsApp.value[group.id]?.$destroy()
-    }
-    else {
-      containerEl = document.createElement('div')
-      containerEl.id = id
-    }
-
-    const app = new EventGroup({
-      target: containerEl,
-      props: { group },
-    })
-
-    app.$on('remove', (e) => {
-      const group = e.detail
-      eventTimelineRef.value?.dataSetGroups.remove(group.id)
-    })
-
-    app.$on('update', (e) => {
-      const group = e.detail
-      const prompt = window.prompt('Update event', group.content)
-
-      if (prompt) {
-        group.content = prompt
-        eventTimelineRef.value?.dataSetGroups.updateOnly(group)
-      }
-    })
-
-    groupsApp.value[group.id] = app
-
-    return containerEl
-  },
-})
+const options = ref<TimelineOptions>()
 
 </script>
 
 <template>
-  <h2>Timeline</h2>
+  <h2 class="font-bold">
+    Timeline Guide
+  </h2>
+  <div class="grid grid-cols-3">
+    <div>
+      <h3 class="font-semibold">
+        Edit group
+      </h3>
+      <p>Add: Add group button</p>
+      <p>Edit: Double click to group name</p>
+      <p>Remove: Click => X</p>
+    </div>
+    <div>
+      <h3 class="font-semibold">
+        Edit item
+      </h3>
+      <p>Add: Window + Drag</p>
+      <p>Edit time: Click => Drag|Resize</p>
+      <p>Edit content: Double click</p>
+      <p>Remove: Click => X</p>
+    </div>
+    <div>
+      <h3 class="font-semibold">
+        Navigation
+      </h3>
+      <p>Group view: Scroll wheel</p>
+      <p>Item view: Shift + wheel</p>
+      <p>Zoom: Ctrll + scroll wheel (configurable: 1h - 1d)</p>
+    </div>
+  </div>
+
   <EventTimeline
     ref="eventTimelineRef"
     v-model:items="items"
--- src/components/EventTimeline.vue
+++ src/components/EventTimeline.vue
@@ -6,6 +6,10 @@ import type {
   DataGroup, DataItem, IdType, TimelineEvents, TimelineOptions,
 } from 'vis-timeline/esnext'
 import { nanoid } from 'nanoid'
+import type { SvelteComponent } from 'svelte'
+import EventTooltip from './EventTooltip.svelte'
+import EventItem from './EventItem.svelte'
+import EventGroup from './EventGroup.svelte'
 
 interface Props {
   groups?: DataGroup[]
@@ -34,6 +38,155 @@ const groupsValue = useVModel(props, 'groups', emit)
 const dataSetItems = ref(new DataSet(props.items))
 const dataSetGroups = ref(new DataSet(props.groups))
 
+const groupsApp = ref<Record<string, SvelteComponent>>({})
+const itemsApp = ref<Record<string, SvelteComponent>>({})
+const tooltipApp = ref<SvelteComponent>()
+
+const options = ref<TimelineOptions>({
+  editable: true,
+  zoomKey: 'ctrlKey',
+  align: 'center',
+  horizontalScroll: true,
+  verticalScroll: true,
+  zoomMin: 1000 * 60 * 60,
+  zoomMax: 1000 * 60 * 60 * 24,
+  // itemsAlwaysDraggable: true,
+  showCurrentTime: true,
+  start: (() => {
+    const date = new Date()
+    date.setHours(date.getHours() - 2)
+    return date
+  })(),
+  end: (() => {
+    const date = new Date()
+    date.setHours(date.getHours() + 2)
+    return date
+  })(),
+  orientation: 'top',
+  minHeight: '150px',
+  maxHeight: '400px',
+  rollingMode: {
+    follow: true,
+    offset: 0.5,
+  },
+  template: (item, element, data) => {
+    if (!item)
+      return ''
+    const id = `item-${item.id}`
+    let containerEl = document.getElementById(id)
+    if (containerEl) {
+      itemsApp.value[item.id]?.$destroy()
+    }
+    else {
+      containerEl = document.createElement('div')
+      containerEl.id = id
+    }
+
+    itemsApp.value[item.id] = new EventItem({
+      target: containerEl,
+      props: { item },
+    })
+
+    return containerEl
+  },
+  tooltipOnItemUpdateTime: {
+    template(item) {
+      const id = `tooltip-${item.id}`
+      let containerEl = document.getElementById(id)
+      if (containerEl) {
+        tooltipApp.value?.$destroy()
+      }
+      else {
+        containerEl = document.createElement('div')
+        containerEl.id = id
+      }
+
+      tooltipApp.value = new EventTooltip({
+        target: containerEl,
+        props: { item },
+      })
+
+      return containerEl.innerHTML
+    },
+  },
+  onAdd: (item, callback) => {
+    const alert = window.prompt('Add event', item.content)
+    if (alert) {
+      item.content = alert
+      callback(item)
+    }
+    else {
+      callback(null)
+    }
+  },
+  onUpdate: (item, callback) => {
+    const alert = window.prompt('Update event', item.content)
+    if (alert) {
+      item.content = alert
+      callback(item)
+    }
+    else {
+      callback(null)
+    }
+  },
+  onMove: (item, callback) => {
+    const confirm = window.confirm('Confirm move')
+    callback(confirm ? item : null)
+  },
+  onRemove(item, callback) {
+    const confirm = window.confirm('Confirm remove')
+    callback(confirm ? item : null)
+  },
+  groupEditable: true,
+  groupOrder: (a, b) => {
+    return a.value - b.value
+  },
+  groupOrderSwap: (a, b, groups) => {
+    const v = a.value
+    a.value = b.value
+    b.value = v
+  },
+  groupTemplate: (group) => {
+    if (!group)
+      return ''
+    const id = `group-${group.id}`
+    let containerEl = document.getElementById(id)
+    if (containerEl) {
+      groupsApp.value[group.id]?.$destroy()
+    }
+    else {
+      containerEl = document.createElement('div')
+      containerEl.id = id
+    }
+
+    const app = new EventGroup({
+      target: containerEl,
+      props: { group },
+    })
+
+    app.$on('remove', (e) => {
+      const group = e.detail
+      const confirm = window.confirm('Confirm remove')
+      if (confirm)
+        dataSetGroups.value.remove(group.id)
+    })
+
+    app.$on('update', (e) => {
+      const group = e.detail
+      const prompt = window.prompt('Update event', group.content)
+
+      if (prompt) {
+        group.content = prompt
+        dataSetGroups.value.updateOnly(group)
+      }
+    })
+
+    groupsApp.value[group.id] = app
+
+    return containerEl
+  },
+})
+
 /** maybe watch individual event to improve performance */
 dataSetItems.value.on('*', (name, payload, senderId) => {
   itemsValue.value = dataSetItems.value.get()
@@ -49,19 +202,22 @@ onMounted(() => {
     timelineRef.value,
     dataSetItems.value,
     dataSetGroups.value,
-    props.options,
+    {
+      ...options.value,
+      ...props.options,
+    },
   )
 })
 
-watch(() => props.options, () => {
-  timeline.value?.setOptions(props.options)
-}, {
-  deep: true,
-})
+// watch(() => props.options, () => {
+//   timeline.value?.setOptions(props.options)
+// }, {
+//   deep: true,
+// })
 
-watch(() => props.selection, (newValue) => {
-  timeline.value?.setSelection(newValue)
-})
+// watch(() => props.selection, (newValue) => {
+//   timeline.value?.setSelection(newValue)
+// })
 
 defineExpose({
   timeline,
@@ -71,8 +227,14 @@ defineExpose({
 
 function addGroup() {
   const prompt = window.prompt('Add group', 'New group')
-  if (prompt)
-    dataSetGroups.value.add({ id: nanoid(), content: prompt })
+  if (prompt) {
+    const value = groupsValue.value.length + 1
+    dataSetGroups.value.add({
+      id: nanoid(),
+      content: `${prompt} ${value}`,
+      value,
+    })
+  }
 }
 
 </script>
